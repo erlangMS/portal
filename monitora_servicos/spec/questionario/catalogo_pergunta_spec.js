93435d1d3359f2a7686ade640b1d7071063d74f0
evertonagilar <evertonagilar@gmail.com>
2016-01-11 17:16:14 -0200

    Pequenos ajustes

--- monitora_servicos/spec/questionario/catalogo_pergunta_spec.js
+++ monitora_servicos/spec/questionario/catalogo_pergunta_spec.js
@@ -27,6 +27,10 @@ describe("Pergunta", function() {
 
 	var testeId = Math.floor(Math.random() * 99999999);
 
+	//
+	// Cadastrar categoria de teste
+	//
+
 	// vamos precisar de uma categoria para a pergunta
 	var objCategoria = {"denominacao" : "Dados Familiares Teste "+ testeId};
 	
@@ -42,89 +46,151 @@ describe("Pergunta", function() {
 				});
 	expect(result.status).toBe(201);
 	expect(result.responseJSON).toBeDefined();
-	
 	var idCategoria = result.responseJSON.id;
+
+	//
+	// Cadastrar pergunta de teste
+	//
 	
-	// Se conseguiu cadastrar uma categoria, seque adiante para cadastrar a pergunta...
-	if (result.status == 201 && idCategoria != undefined){
-
-		// define os dados do cadastro
-		var objPergunta = {"enunciado":"Voce tem moradia própria? Teste "+ testeId, 
-						   "tipoResposta":1, 
-						   "categoria": idCategoria}
+	// define os dados do cadastro
+	var objPergunta = {"enunciado":"Voce tem moradia própria? Teste "+ testeId, 
+					   "tipoResposta":2,  // tipo escolha uma
+					   "categoria": idCategoria}
 
 
-		// tenta incluir pergunta		   
-		result = $.ajax({
-						url:  "http://localhost:2301/questionario/pergunta",
-						data : JSON.stringify(objPergunta),
-						type: "POST",
-						contentType: "application/x-www-form-urlencoded; charset=UTF-8",
-						dataType: "json",
-						crossDomain: true,
-						async: false
-					});
-		expect(result.status).toBe(201);
-		expect(result.responseJSON).toBeDefined();
-
-		var objInserido = result.responseJSON;
-
-		// se incluiu, tenta modificar o registro
-		if (result.status == 201 && objInserido.id != undefined){
-			
-				// vamos fazer uma modificação no registro
-				objUpdate = objInserido;
-				objUpdate.enunciado = objUpdate.enunciado + "(update)";
-				objUpdate.categoria = idCategoria;
-				objUpdate.tipoResposta = 2;
-
-				// tenta modificar
-				result = $.ajax({
-								url:  "http://localhost:2301/questionario/pergunta/"+ objUpdate.id,
-								data : JSON.stringify(objUpdate),
-								type: "PUT",
-								contentType: "application/x-www-form-urlencoded; charset=UTF-8",
-								dataType: "json",
-								crossDomain: true,
-								async: false
-							});
-				expect(result.status).toBe(200);
-
-				// faz a pesquisa
-				result = $.ajax({
-								url:  "http://localhost:2301/questionario/pergunta/"+ objInserido.id,
-								type: "GET",
-								contentType: "application/x-www-form-urlencoded; charset=UTF-8",
-								dataType: "json",
-								crossDomain: true,
-								async: false
-							});
-				expect(result.status).toBe(200);
+	// tenta incluir pergunta		   
+	result = $.ajax({
+					url:  "http://localhost:2301/questionario/pergunta",
+					data : JSON.stringify(objPergunta),
+					type: "POST",
+					contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+					dataType: "json",
+					crossDomain: true,
+					async: false
+				});
+	expect(result.status).toBe(201);
+	expect(result.responseJSON).toBeDefined();
+	var idPergunta = result.responseJSON.id;
+	var objInserido = result.responseJSON;
 
 
-				// vamos apagar o registro do teste da pergunta
-				result = $.ajax({
-								url:  "http://localhost:2301/questionario/pergunta/"+ objInserido.id,
-								type: "DELETE",
-								contentType: "application/x-www-form-urlencoded; charset=UTF-8",
-								dataType: "json",
-								crossDomain: true,
-								async: false
-							});
-				expect(result.status).toBe(200);
-		}
+	//
+	// Cadastra as respostas sim e não
+	//
+
+	var objRespostaSim = {"descricao":"Sim", 
+						  "pergunta":idPergunta,
+						  "valorResposta": 1}
+
+	var objRespostaNao = {"descricao":"Não", 
+						  "pergunta":idPergunta,
+						  "valorResposta": 2}
+
+	// tenta incluir resposta sim
+	result = $.ajax({
+					url:  "http://localhost:2301/questionario/pergunta/"+ idPergunta + "/resposta",
+					data : JSON.stringify(objRespostaSim),
+					type: "POST",
+					contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+					dataType: "json",
+					crossDomain: true,
+					async: false
+				});
+	expect(result.status).toBe(201);
+	expect(result.responseJSON).toBeDefined();
+	var idRespostaSim = result.responseJSON.id;
+
+	// tenta incluir resposta não
+	result = $.ajax({
+					url:  "http://localhost:2301/questionario/pergunta/"+ idPergunta + "/resposta",
+					data : JSON.stringify(objRespostaNao),
+					type: "POST",
+					contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+					dataType: "json",
+					crossDomain: true,
+					async: false
+				});
+	expect(result.status).toBe(201);
+	expect(result.responseJSON).toBeDefined();
+	var idRespostaNao = result.responseJSON.id;
 		
-		// vamos apagar o registro do teste da categoria
-		result = $.ajax({
-						url:  "http://localhost:2301/questionario/categoria_pergunta/"+ idCategoria,
-						type: "DELETE",
-						contentType: "application/x-www-form-urlencoded; charset=UTF-8",
-						dataType: "json",
-						crossDomain: true,
-						async: false
-					});
-		expect(result.status).toBe(200);
-	}
+
+	//
+	// Modificar a pergunta
+	//
+
+	// vamos fazer uma modificação na pergunta
+	objUpdate = objInserido;
+	objUpdate.enunciado = objUpdate.enunciado + "(update)";
+	objUpdate.categoria = idCategoria;
+	objUpdate.tipoResposta = 2;
+
+	// tenta modificar
+	result = $.ajax({
+					url:  "http://localhost:2301/questionario/pergunta/"+ objUpdate.id,
+					data : JSON.stringify(objUpdate),
+					type: "PUT",
+					contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+					dataType: "json",
+					crossDomain: true,
+					async: false
+				});
+	expect(result.status).toBe(200);
+
+	// faz a pesquisa
+	result = $.ajax({
+					url:  "http://localhost:2301/questionario/pergunta/"+ objInserido.id,
+					type: "GET",
+					contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+					dataType: "json",
+					crossDomain: true,
+					async: false
+				});
+	expect(result.status).toBe(200);
+
+	// vamos apagar a resposta sim
+	result = $.ajax({
+					url:  "http://localhost:2301/questionario/pergunta/"+ idPergunta + "/resposta/"+ idRespostaSim,
+					type: "DELETE",
+					contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+					dataType: "json",
+					crossDomain: true,
+					async: false
+				});
+	expect(result.status).toBe(200);
+
+	// vamos apagar a resposta não
+	result = $.ajax({
+					url:  "http://localhost:2301/questionario/pergunta/"+ idPergunta + "/resposta/"+ idRespostaNao,
+					type: "DELETE",
+					contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+					dataType: "json",
+					crossDomain: true,
+					async: false
+				});
+	expect(result.status).toBe(200);
+
+	// vamos apagar o registro do teste da pergunta
+	result = $.ajax({
+					url:  "http://localhost:2301/questionario/pergunta/"+ objInserido.id,
+					type: "DELETE",
+					contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+					dataType: "json",
+					crossDomain: true,
+					async: false
+				});
+	expect(result.status).toBe(200);
+	
+	// vamos apagar o registro do teste da categoria
+	result = $.ajax({
+					url:  "http://localhost:2301/questionario/categoria_pergunta/"+ idCategoria,
+					type: "DELETE",
+					contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+					dataType: "json",
+					crossDomain: true,
+					async: false
+				});
+	expect(result.status).toBe(200);
  });
 
 
--- monitora_servicos/spec/sae/catalogo_estudo_socioeconomico_spec.js
+++ monitora_servicos/spec/sae/catalogo_estudo_socioeconomico_spec.js
@@ -27,6 +27,10 @@ describe("EstudoSocioEconomico", function() {
 
 	var testeId = Math.floor(Math.random() * 99999999);
 
+	//
+	// Cadastrar aluno para teste
+	//
+
 	// define os dados do aluno
 	var objAluno = {"nome":"Aluno Teste "+ testeId,
 					"cpf":"1111111111",
@@ -46,6 +50,10 @@ describe("EstudoSocioEconomico", function() {
 	expect(result.responseJSON).toBeDefined();
 	var idAluno = result.responseJSON.id;
 
+	//
+	// Cadastro questionário para teste
+	//
+
 	// define os dados do cadastro de questionário
 	var objQuestionario = {"denominacao":"questionário de teste "+ testeId,
 						   "dataInicio":"01/12/2015", 
@@ -61,11 +69,101 @@ describe("EstudoSocioEconomico", function() {
 					crossDomain: true,
 					async: false
 				});
-
 	expect(result.status).toBe(201);
 	expect(result.responseJSON).toBeDefined();
 	var idQuestionario = result.responseJSON.id;
 
+	// precisamos cadastrar primeiramente uma categoria de pergunta
+	var objCategoria = {"denominacao" : "Dados Familiares Teste. TesteId " + testeId};
+	
+	// tenta incluir categoria da pergunta
+	result = $.ajax({
+					url:  "http://localhost:2301/questionario/categoria_pergunta",
+					data : JSON.stringify(objCategoria),
+					type: "POST",
+					contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+					dataType: "json",
+					crossDomain: true,
+					async: false
+				});
+	expect(result.status).toBe(201);
+	expect(result.responseJSON).toBeDefined();
+	var idCategoria = result.responseJSON.id;
+	
+	// precisamos criar duas peguntas de teste para vincular ao questionário
+	// define os dados da pergunta
+	var objPergunta = {"enunciado":"Voce tem moradia própria? pergunta 01 do teste. Testeid "+ testeId, 
+					   "tipoResposta":1, 
+					   "categoria": idCategoria}
+
+
+	// tenta incluir pergunta 01
+	result = $.ajax({
+					url:  "http://localhost:2301/questionario/pergunta",
+					data : JSON.stringify(objPergunta),
+					type: "POST",
+					contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+					dataType: "json",
+					crossDomain: true,
+					async: false
+				});
+	expect(result.status).toBe(201);
+	expect(result.responseJSON).toBeDefined();
+	var idPergunta1 = result.responseJSON.id;
+		
+		
+	// define os dados da pergunta 02
+	var objPergunta2 = {"enunciado":"Qual a sua escolaridade? pergunta 02 do teste. TesteId "+ testeId, 
+						"tipoResposta":1, 
+						"categoria": idCategoria}
+
+
+	// tenta incluir pergunta		   
+	result = $.ajax({
+					url:  "http://localhost:2301/questionario/pergunta",
+					data : JSON.stringify(objPergunta2),
+					type: "POST",
+					contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+					dataType: "json",
+					crossDomain: true,
+					async: false
+				});
+	expect(result.status).toBe(201);
+	expect(result.responseJSON).toBeDefined();
+	var idPergunta2 = result.responseJSON.id;
+	
+	// finalmente, tenta vincular a pergunta 01 ao questionário
+	result = $.ajax({
+					url:  "http://localhost:2301/questionario/questionario/" + idQuestionario +  "/pergunta",
+					data : JSON.stringify({"pergunta" : idPergunta1}),
+					type: "POST",
+					contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+					dataType: "json",
+					crossDomain: true,
+					async: false
+				});
+	expect(result.status).toBe(201);
+	expect(result.responseJSON).toBeDefined();
+			
+
+	// vamos vinvular a pergunta 02..
+	result = $.ajax({
+					url:  "http://localhost:2301/questionario/questionario/" + idQuestionario +  "/pergunta",
+					data : JSON.stringify({"pergunta" : idPergunta2}),
+					type: "POST",
+					contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+					dataType: "json",
+					crossDomain: true,
+					async: false
+				});
+	expect(result.status).toBe(201);
+	expect(result.responseJSON).toBeDefined();
+
+
+	//
+	// cadastro do estudo socioeconomico
+	// 
+	
 
 	// define os dados do cadastro estudo socioeconomico
 	var obj = {"periodo":"20141", 
@@ -85,54 +183,148 @@ describe("EstudoSocioEconomico", function() {
 				});
 	expect(result.status).toBe(201);
 	expect(result.responseJSON).toBeDefined();
-
 	var objInserido = result.responseJSON;
 
-	// se incluiu, tenta modificar o registro
-	if (result.status == 201 && objInserido.id != undefined){
+	objUpdate = objInserido;
+	objUpdate.dataHora = "01/01/2015";
+	objUpdate.periodo = "20151";
+	objUpdate.aluno = idAluno; 
+	objUpdate.questionario = idQuestionario;
 
-			objUpdate = objInserido;
-			objUpdate.dataHora = "01/01/2015";
-			objUpdate.periodo = "20151";
-			objUpdate.aluno = idAluno; 
-			objUpdate.questionario = idQuestionario;
+	// tenta modificar		   
+	result = $.ajax({
+					url:  "http://localhost:2301/sae/estudo/socioeconomico/"+ objInserido.id,
+					data : JSON.stringify(objUpdate),
+					type: "PUT",
+					contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+					dataType: "json",
+					crossDomain: true,
+					async: false
+				});
+	expect(result.status).toBe(200);
 
-			// tenta modificar		   
-			result = $.ajax({
-							url:  "http://localhost:2301/sae/estudo/socioeconomico/"+ objInserido.id,
-							data : JSON.stringify(objUpdate),
-							type: "PUT",
-							contentType: "application/x-www-form-urlencoded; charset=UTF-8",
-							dataType: "json",
-							crossDomain: true,
-							async: false
-						});
-			expect(result.status).toBe(200);
+	// tenta pesquisar
+	result = $.ajax({
+					url:  "http://localhost:2301/sae/estudo/socioeconomico/"+ objInserido.id,
+					data : JSON.stringify(objUpdate),
+					type: "GET",
+					contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+					dataType: "json",
+					crossDomain: true,
+					async: false
+				});
+	expect(result.status).toBe(200);
 
-			// tenta pesquisar
-			result = $.ajax({
-							url:  "http://localhost:2301/sae/estudo/socioeconomico/"+ objInserido.id,
-							data : JSON.stringify(objUpdate),
-							type: "GET",
-							contentType: "application/x-www-form-urlencoded; charset=UTF-8",
-							dataType: "json",
-							crossDomain: true,
-							async: false
-						});
-			expect(result.status).toBe(200);
+	// responder a pergunta 01
+	var objResposta01 = {"estudo":idEstudo, 
+				         "dataHora":"01/01/2014", 
+						 "pergunta":idPergunta01, 
+						 "resposta":idResposta01}
 
-			// modificação feita, vamos apagar o registro do estudo de teste
-			result = $.ajax({
-							url:  "http://localhost:2301/sae/estudo/socioeconomico/"+ objInserido.id,
-							type: "DELETE",
-							contentType: "application/x-www-form-urlencoded; charset=UTF-8",
-							dataType: "json",
-							crossDomain: true,
-							async: false
-						});
-			expect(result.status).toBe(200);
-	}
+	result = $.ajax({
+					url:  "http://localhost:2301/sae/estudo/socioeconomico/"+ objInserido.id + "/resposta",
+					data : JSON.stringify(objUpdate),
+					type: "POST",
+					contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+					dataType: "json",
+					crossDomain: true,
+					async: false
+				});
+	expect(result.status).toBe(200);
 
+	//
+	// Vamos apagar os registros do estudo socioeconomico
+	//
+	
+	// modificação feita, vamos apagar o registro do estudo de teste
+	result = $.ajax({
+					url:  "http://localhost:2301/sae/estudo/socioeconomico/"+ objInserido.id,
+					type: "DELETE",
+					contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+					dataType: "json",
+					crossDomain: true,
+					async: false
+				});
+	expect(result.status).toBe(200);
+
+
+	// 
+	// vamos apagar o questionário, desvincular as perguntas, apagar as perguntas e a categoria da pergunta...
+	//
+
+	// desvincular a pergunta 01 do questionário
+	result = $.ajax({
+					url:  "http://localhost:2301/questionario/questionario/" + idQuestionario +  "/pergunta/"+ idPergunta1,
+					type: "DELETE",
+					contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+					dataType: "json",
+					crossDomain: true,
+					async: false
+				});
+	expect(result.status).toBe(200);
+	expect(result.responseJSON).toBeDefined();
+
+	// desvincular a pergunta 02 do questionário
+	result = $.ajax({
+					url:  "http://localhost:2301/questionario/questionario/" + idQuestionario +  "/pergunta/"+ idPergunta2,
+					type: "DELETE",
+					contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+					dataType: "json",
+					crossDomain: true,
+					async: false
+				});
+	expect(result.status).toBe(200);
+	expect(result.responseJSON).toBeDefined();
+
+	// apagar o questionário
+	result = $.ajax({
+					url:  "http://localhost:2301/questionario/questionario/"+ idQuestionario,
+					type: "DELETE",
+					contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+					dataType: "json",
+					crossDomain: true,
+					async: false
+				});
+	expect(result.status).toBe(200);
+
+	// apagar a primeira pergunta
+	result = $.ajax({
+					url:  "http://localhost:2301/questionario/pergunta/"+ idPergunta1,
+					type: "DELETE",
+					contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+					dataType: "json",
+					crossDomain: true,
+					async: false
+				});
+	expect(result.status).toBe(200);
+
+	// apagar a segunda pergunta
+	result = $.ajax({
+					url:  "http://localhost:2301/questionario/pergunta/"+ idPergunta2,
+					type: "DELETE",
+					contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+					dataType: "json",
+					crossDomain: true,
+					async: false
+				});
+	expect(result.status).toBe(200);
+
+	// apagar a categoria da pergunta
+	result = $.ajax({
+					url:  "http://localhost:2301/questionario/categoria_pergunta/"+ idCategoria,
+					type: "DELETE",
+					contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+					dataType: "json",
+					crossDomain: true,
+					async: false
+				});
+	expect(result.status).toBe(200);
+
+	
+	// 
+	// vamos apagar o aluno de teste
+	//
+	
 	// vamos apagar o registro do aluno de teste
 	result = $.ajax({
 					url:  "http://localhost:2301/sigra/aluno/"+ idAluno,
@@ -143,17 +335,8 @@ describe("EstudoSocioEconomico", function() {
 					async: false
 				});
 	expect(result.status).toBe(200);
+
 	
-	// vamos apagar o registro do questionário de teste
-	result = $.ajax({
-					url:  "http://localhost:2301/questionario/questionario/"+ idQuestionario,
-					type: "DELETE",
-					contentType: "application/x-www-form-urlencoded; charset=UTF-8",
-					dataType: "json",
-					crossDomain: true,
-					async: false
-				});
-	expect(result.status).toBe(200);
 	
  });
 
